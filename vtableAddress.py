5c4bdb703edad566b8257dba2090d59b67119fcc
Dudcom <70006792+Dudcom@users.noreply.github.com>
2024-12-25 19:25:32 -0500

    Update vtableAddress.py
    fixed

--- vtableAddress.py
+++ vtableAddress.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import idc
 import idautils
-import ida_frame
-import ida_struct
 import idaapi
-import sys, os
+import ida_ida
+import ida_typeinf
+import sys
+import os
 
 idaapi.require("AddBP")
 
@@ -14,28 +15,47 @@ REGISTERS = ['eax', 'ebx', 'ecx', 'edx', 'rax', 'rbx', 'rcx', 'rdx', 'rdi', 'rsi
 
 
 def get_processor_architecture():
+    """
+    Detect processor architecture and bitness.
+    """
     arch = "Intel"
-    info = idaapi.get_inf_structure()
-    if info.procName == "ARM":
+    proc_name = ida_ida.inf_get_procname()  # Replacement for procname
+    if proc_name == "ARM":
         arch = "ARM"
-    if info.is_64bit():
+    if ida_ida.inf_is_64bit():  # Replacement for is_64bit()
         return arch, True
-    elif info.is_32bit():
+    elif ida_ida.inf_is_32bit():  # Replacement for is_32bit()
         return arch, False
     else:
         return "Error", False
 
 
 def get_local_var_value_64(loc_var_name):
-    frame = ida_frame.get_frame(idc.here())
-    loc_var = ida_struct.get_member_by_name(frame, loc_var_name)
-    loc_var_start = loc_var.soff
+    """
+    Retrieve the value of a local variable for 64-bit architectures.
+    """
+    frame_id = idc.get_frame_id(idc.here())
+    if frame_id == idc.BADADDR:
+        print("Error: Unable to retrieve frame.")
+        return None
+    frame = ida_typeinf.tinfo_t()
+    if not frame.get_numbered_type(idaapi.cvar.idati, frame_id):
+        print("Error: Unable to load frame structure.")
+        return None
+    member = frame.get_udm_by_name(loc_var_name)
+    if not member:
+        print(f"Error: Local variable '{loc_var_name}' not found.")
+        return None
+    loc_var_start = member.offset
     loc_var_ea = loc_var_start + idc.get_reg_value("RSP")
-    loc_var_value = idc.read_dbg_qword(loc_var_ea)  # in case the variable is 32bit, just use get_wide_dword() instead
+    loc_var_value = idc.read_dbg_qword(loc_var_ea)
     return loc_var_value
 
 
 def get_arch_dct():
+    """
+    Define architecture-specific details for ARM and Intel.
+    """
     arch, is_64 = get_processor_architecture()
     if arch != "Error" or (arch == "ARM" and not is_64):
         dct_arch = {}
@@ -49,65 +69,48 @@ def get_arch_dct():
             dct_arch["val_offset"] = 1
         return dct_arch
     else:
-        print("Error, Architecture is not supported. Supported architectures are Intel x64/x32 and Arm x64")
+        print("Error: Unsupported architecture. Supported architectures are Intel x64/x32 and ARM x64.")
         return -1
 
 
 def get_con2_var_or_num(i_cnt, cur_addr):
     """
-    :param i_cnt: the register of the virtual call
-    :param cur_addr: the current address in the memory
-    :return: "success" string and the address of the vtable's location. if it fails it sends the reason and -1
+    Get information about virtual calls in a function.
     """
     start_addr = idc.get_func_attr(cur_addr, idc.FUNCATTR_START)
-    virt_call_addr = cur_addr
-    cur_addr = idc.prev_head(cur_addr)
     dct_arch = get_arch_dct()
     if dct_arch == -1:
-        return 'Wrong Architechture', "-1", cur_addr
+        return 'Unsupported Architecture', "-1", cur_addr
 
     while cur_addr >= start_addr:
-        if idc.print_insn_mnem(cur_addr)[:3] == dct_arch["opcode"] and idc.print_operand(cur_addr, 0) == i_cnt:  # TODO lea ?
+        if idc.print_insn_mnem(cur_addr)[:3] == dct_arch["opcode"] and idc.print_operand(cur_addr, 0) == i_cnt:
             opnd2 = idc.print_operand(cur_addr, 1)
             place = opnd2.find(dct_arch["separator"])
-            if place != -1:  # if the function is not the first in the vtable
+            if place != -1:
                 register = opnd2[opnd2.find('[') + 1: place]
-                if opnd2.find('*') == -1:
-                    offset = opnd2[place + dct_arch["val_offset"]: opnd2.find(']')]
-                else:
-                    offset = "*"
+                offset = opnd2[place + dct_arch["val_offset"]: opnd2.find(']')]
                 return register, offset, cur_addr
             else:
                 offset = "0"
-                if opnd2.find(']') != -1:
-                    register = opnd2[opnd2.find('[') + 1: opnd2.find(']')]
-                else:
-                    register = opnd2
+                register = opnd2[opnd2.find('[') + 1: opnd2.find(']')]
                 return register, offset, cur_addr
         elif idc.print_insn_mnem(cur_addr)[:4] == "call":
             intr_func_name = idc.print_operand(cur_addr, 0)
-            # In case the code has CFG -> ignores the function call before the virtual calls
             if "guard_check_icall_fptr" not in intr_func_name:
-                if "nullsub" not in intr_func_name:
-                    # intr_func_name = idc.Demangle(intr_func_name, idc.GetLongPrm(idc.INF_SHORT_DN))
-                    print("Warning! At address 0x%08x: The vtable assignment might be in another function (Maybe %s),"
-                          " could not place BP." % (virt_call_addr, intr_func_name))
+                print(f"Warning: Virtual call might be in another function near {hex(cur_addr)}.")
                 cur_addr = start_addr
         cur_addr = idc.prev_head(cur_addr)
-    return "out of the function", "-1", cur_addr
-
-    return '', 0, cur_addr
+    return "Out of function", "-1", cur_addr
 
 
 def get_bp_condition(start_addr, register_vtable, offset):
+    """
+    Generate breakpoint condition script for the given architecture.
+    """
     arch, is_64 = get_processor_architecture()
-    file_name = "BPCond.py"
-    if arch == "Intel":
-        if is_64:
-            file_name = "BPCond64.py"
-    else:
-        file_name = "BPCondAarch64.py"
+    file_name = "BPCond.py" if arch == "Intel" else "BPCondAarch64.py"
     condition_file = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), file_name)
+
     if arch != "Error" or (arch == "ARM" and not is_64):
         with open(condition_file, 'r') as f1:
             bp_cond_text = f1.read()
@@ -120,43 +123,40 @@ def get_bp_condition(start_addr, register_vtable, offset):
 
 def write_vtable2file(start_addr):
     """
-    :param start_addr: The start address of the virtual call
-    :return: The break point condition and the break point address
+    Write the vtable information to a file and return the breakpoint condition and address.
     """
     raw_opnd = idc.print_operand(start_addr, 0)
-    if raw_opnd in REGISTERS:
-        reg = raw_opnd
-    else:
-        for reg in REGISTERS:
-            if raw_opnd.find(reg) != -1:
-                break
+    reg = next((reg for reg in REGISTERS if reg in raw_opnd), None)
     opnd = get_con2_var_or_num(reg, start_addr)
     reg_vtable = opnd[0]
     offset = opnd[1]
     bp_address = opnd[2]
     set_bp = True
     cond = ""
-    # TODO check the get_con2 return variables!!@
     try:
-        # TODO If a structure was already assigned to the BP (not by Virtuailor), before running the code the code will\
-        # assume it was examined by the user, the BP will not be set
         arch_dct = get_arch_dct()
-        plus_indx = raw_opnd.find(arch_dct["separator"])
-        if plus_indx != -1:
-            call_offset = raw_opnd[plus_indx + 1:raw_opnd.find(']')]
-            # if the offset is in hex
-            if call_offset.find('h') != -1:
+        plus_index = raw_opnd.find(arch_dct["separator"])
+        if plus_index != -1:
+            call_offset = raw_opnd[plus_index + 1:raw_opnd.find(']')]
+            if 'h' in call_offset:
                 call_offset = int(call_offset[:call_offset.find('h')], 16)
-        if offset.find('h') != -1:
+        if 'h' in offset:
             offset = str(int(offset[:offset.find('h')], 16))
-        elif offset.find('0x') != -1:
-            offset = str(int(offset[offset.find('0x') + 2:], 16))
     except ValueError:
-        # A offset structure was set, the old offset will be deleted
         set_bp = False
     finally:
         if set_bp:
-            # start_addr = start_addr - idc.SegStart(start_addr)
             if reg_vtable in REGISTERS:
                 cond = get_bp_condition(start_addr, reg_vtable, offset)
     return cond, bp_address
+
+
+if __name__ == "__main__":
+    try:
+        start_addr_range = ida_ida.inf_get_min_ea()
+        end_addr_range = ida_ida.inf_get_max_ea()
+        arch, is_64 = get_processor_architecture()
+        print(f"Architecture: {arch}, 64-bit: {is_64}")
+        print(f"Address range: {hex(start_addr_range)} - {hex(end_addr_range)}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
